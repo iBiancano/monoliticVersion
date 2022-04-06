832445d6de75cc3c2322d419aecd14c628bcd599
IVAN BLANCAS RIVERA <L17650013@zitacuaro.tecnm.mx>
2022-04-05 20:48:31 -0500

    Initial version of Prueba_Monolitica generated by generator-jhipster@7.8.0

---
+++ src/test/java/com/mycompany/myapp/IntegrationTest.java
@@ -0,0 +1,19 @@
+package com.mycompany.myapp;
+
+import com.mycompany.myapp.PruebaMonoliticaApp;
+import com.mycompany.myapp.config.EmbeddedMongo;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = PruebaMonoliticaApp.class)
+@EmbeddedMongo
+public @interface IntegrationTest {
+}
